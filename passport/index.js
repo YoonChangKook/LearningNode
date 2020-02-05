78ac5d4f3bae9e00a4b863515790c20084066346
YoonChang Kook <yc.kook@navercorp.com>
2020-02-05 21:09:11 +0900

    :sparkles: Add local authentication

---
+++ passport/index.js
@@ -0,0 +1,23 @@
+const local = require('./localStrategy')
+//const kakao = require('./kakaoStrategy')
+//const naver = require('./naverStrategy')
+const { User } = require('../models')
+
+module.exports = passport => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({ where: { id } })
+      done(null, user)
+    } catch (err) {
+      done(err)
+    }
+  })
+
+  local(passport)
+  //kakao(passport)
+  //naver(passport)
+}
